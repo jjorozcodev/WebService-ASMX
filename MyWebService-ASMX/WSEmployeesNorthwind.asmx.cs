300a7042d5b2961e85ae5f549ca878f6ab2a0fd1
JJ <jj.orozcolopez.13@gmail.com>
2019-07-13 06:37:14 -0600

    Integrating WebService (ASMX)
    Added item "Web Service (ASMX)" to the initial project.
    The webservice will be used to consuming the employees' data from Northwind database.

---
+++ MyWebService-ASMX/WSEmployeesNorthwind.asmx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace MyWebService_ASMX
+{
+    /// <summary>
+    /// Summary description for WSEmployeesNorthwind
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WSEmployeesNorthwind : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
